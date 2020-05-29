2f43d73d4c79ed28a64654a84a8e88fb1f6ebab7
Raul Palacios <palacios.raul02@gmail.com>
2020-05-28 22:32:19 -0500

    Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
+	import About from './components/About.svelte'
 	export let name;
 	let svelteLogo = "https://arepa.s3.amazonaws/svelte-logo.png"
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>FullStack Software Engineer</p>
+	<About />
 	<img src={svelteLogo} alt="Svelte">
 </main>
 
@@ -18,17 +19,13 @@
 		--theme-color: purple;
 	}
 
-	p{
-		color: var(--theme-color);
-		font-size: 24px;
-	}
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-	
+
 	main img {
 		height: 30%;
 	}
---
+++ src/components/About.svelte
@@ -0,0 +1,14 @@
+<script>
+let someText = "FullStack Software Engineer"
+</script>
+
+<div class="about">
+    <p>{someText}</p>
+</div>
+
+<style>
+p{
+    color: var(--theme-color);
+    font-size: 24px;
+}
+</style>
