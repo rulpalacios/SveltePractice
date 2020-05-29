c5847fee242ff8e7ec980e4797a6f48144617d38
Raul Palacios <palacios.raul02@gmail.com>
2020-05-28 22:40:43 -0500

    Reactive

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,19 @@
 <script>
 let someText = "FullStack Software Engineer"
+let count = 0
+
+const handleClick = () => count += 1
+
+// function handleClick(){
+//     count += 1
+// }
+
 </script>
 
 <div class="about">
     <p>{someText}</p>
+    <button on:click={handleClick}>Commit</button>
+    <p>Haz hecho {count} {count === 1 ? 'commit' : 'commits'}</p>
 </div>
 
 <style>
