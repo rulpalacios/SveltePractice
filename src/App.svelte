10848bb8c7ed869b94ed1f5cddb3e0d0f92f746d
Raul Palacios <palacios.raul02@gmail.com>
2020-05-28 22:20:53 -0500

    Bootstrap Project

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let name;
+	let svelteLogo = "https://arepa.s3.amazonaws/svelte-logo.png"
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>FullStack Software Engineer</p>
+	<img src={svelteLogo} alt="Svelte">
+</main>
+
+<style>
+	:global(body){
+		background-color: #f2eee2;
+		color: #0084f6
+	}
+	:global(:root){
+		--theme-color: purple;
+	}
+
+	p{
+		color: var(--theme-color);
+		font-size: 24px;
+	}
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	
+	main img {
+		height: 30%;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
